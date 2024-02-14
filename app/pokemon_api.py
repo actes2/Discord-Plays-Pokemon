563727183068dde229faddbc9e419255edbdb4c5
actes2 <actesbusiness@gmail.com>
2024-02-13 22:06:19 -0600

    work in progress

--- app/pokemon_api.py
+++ app/pokemon_api.py
@@ -1,12 +1,40 @@
 import pyautogui
 import discord
+import asyncio
+import threading
+
+api_key = "MTIwNzA0Mzk3OTA0NTExMzkyOA.GGefJv.oW74Bj1A-EqEjwhgtv33XK84xfiWLGwj9cBkVs"
+channel_id = "1206692419999891506"
+
+thread_killer = True
+last_command = ""
+
+class discordClient(discord.Client):
+    async def on_ready(self):
+        print("live and kicking")
+    
+    async def send_message(self, message_to_send):
+        channel = self.get_channel(channel_id)
+        await channel.send(message_to_send)
+
+bot = discordClient(intents=None)
+
+def bot_thread():
+    
+    bot.run(api_key)
 
 
 def main():
-    pass
+    print("init")
 
-
-if __name__ is "__main__":
+    thread = threading.Thread(target=bot_thread)
+    thread.start()
+    while True:
+        pass
     
-    print("Hello world!")
+    exit()
+
+
+
+if __name__ == "__main__":
     main()
