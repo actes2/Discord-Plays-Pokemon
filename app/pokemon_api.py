f4c319a747d22a437cef1f1b2a8c7f45918c8b39
actes2 <actesbusiness@gmail.com>
2024-02-14 18:40:04 -0600

    lots of changes

--- app/pokemon_api.py
+++ app/pokemon_api.py
@@ -1,4 +1,4 @@
-import pyautogui
+import pyautogui as pyg
 import discord
 import asyncio
 import threading
@@ -14,17 +14,56 @@ class DiscordClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.skipper = False
+
+    async def game(self):
+        print("Game init")
+        
+        self.skipper = True
+        channel = self.get_channel(channel_id)
+
+        gamewin = pyg.getWindowsWithTitle("mGBA")[0]
+        screen = pyg.screenshot().crop([gamewin.left + 8, gamewin.top + 52, gamewin.right - 8, gamewin.bottom - 8])        
+        screen.save("frame.png")
+
+        frame = await channel.send(file=discord.File("frame.png"))
+
+        await asyncio.sleep(3)
+        await self.fastnuke()
+        
+    async def fastnuke(self):
+        print("Wiping Frame")
+        channel = self.get_channel(channel_id)
+        
+        messages = channel.history(limit=None, oldest_first=True)
+        async for msg in messages:
+            if not msg.author.name == "actes_plays_pokemon":
+                await msg.delete()
+
+
     async def nuke(self):
         print("Nuking channel")
         channel = self.get_channel(channel_id)
         await channel.send("Nuking the entire channel in 5")
         await asyncio.sleep(5)
         
+        print(len(channel.history))
         async for i in channel.history(limit=None):
             await i.delete()
+
+    async def screenshot(self):
+        print("Taking screenshot")
+        channel = self.get_channel(channel_id)
+
+        gamewin = pyg.getWindowsWithTitle("mGBA")[0]
+        screen = pyg.screenshot().crop([gamewin.left + 8, gamewin.top + 52, gamewin.right - 8, gamewin.bottom - 8])
+        screen.save("frame.png")
+
+        frame = await channel.send(file=discord.File("frame.png"))
+        await asyncio.sleep(3)
+        await frame.delete()
+
         
-
-
     async def on_ready(self):
         print("Ready for action")
         channel = self.get_channel(channel_id)
@@ -32,9 +71,14 @@ class DiscordClient(discord.Client):
 
         # message_buffer = []
         for x in message_buffer:
+            if x == "/game":
+                await self.game()
+
             if x == "/nuke":
                 await self.nuke()
-                
+            
+            if x == "/ss":
+                await self.screenshot()
 
             if "/send-" in x:
                 
@@ -54,9 +98,15 @@ class DiscordClient(discord.Client):
                 
 
         print("Clearing buffer and closing connection")
+        
 
         message_buffer.clear()
         await self.close()
+        await self.http.close()
+        asyncio.get_event_loop().close()
+        
+        
+            
 
 # @client.event
 # async def on_ready():
@@ -77,26 +127,41 @@ def main():
     
     # thread1 = threading.Thread(target=bot_execution_thread)
     # thread1.start()
-
+    gamer = False
 
     while True:
-        message = input("Message here:")
-
-        if message == 'kill':
-            thread_killer = False
-            print("killed all threads")
-        
-        if message == "/nuke":
-
+        if gamer:
             message_buffer.append(message)
             client = DiscordClient(intents=discord.Intents.default())
             client.run(api_key, reconnect=False)
+        else:
+            message = input("Message here:")
+            
+            if message == "/game":
+                gamer = True
 
-        if "/send-" in message:
+            if message == 'kill':
+                thread_killer = False
+                print("killed all threads")
+            
+            if message == "/ss":
+                message_buffer.append(message)
+                client = DiscordClient(intents=discord.Intents.default())
+                client.run(api_key, reconnect=False)
 
-            message_buffer.append(message)
-            client = DiscordClient(intents=discord.Intents.default())
-            client.run(api_key, reconnect=False)
+            if message == "/nuke":
+
+                message_buffer.append(message)
+                client = DiscordClient(intents=discord.Intents.default())
+                client.run(api_key, reconnect=False)
+
+            if "/send-" in message:
+
+                message_buffer.append(message)
+                client = DiscordClient(intents=discord.Intents.default())
+                client.run(api_key, reconnect=False)
+                
+                
             
 
             
