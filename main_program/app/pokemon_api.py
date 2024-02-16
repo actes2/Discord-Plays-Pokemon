fc4d9526d67ab187a6d17e5566efbd8df10dd23c
actes2 <actesbusiness@gmail.com>
2024-02-15 19:18:09 -0600

    Huge changes to the api. We're now just running threads - thank heavens

--- main_program/app/pokemon_api.py
+++ main_program/app/pokemon_api.py
@@ -4,10 +4,10 @@ import asyncio
 import threading
 import logging
 import subprocess
+import dotenv
 import sys
 
 logger = logging.getLogger()
-
 api_key = "MTIwNzA0Mzk3OTA0NTExMzkyOA.GGefJv.oW74Bj1A-EqEjwhgtv33XK84xfiWLGwj9cBkVs"
 channel_id = 1206692419999891506
 
@@ -42,6 +42,7 @@ class DiscordClient(discord.Client):
         super().__init__(*args, **kwargs)
 
         self.skipper = False
+        self.command = ""
 
     # we're gonna try and get it to just edit the last message with the new screenshot as to create a more 'game-esk' screen
     async def game(self):
@@ -49,6 +50,8 @@ class DiscordClient(discord.Client):
         
         self.skipper = True
         channel = self.get_channel(channel_id)
+        
+        
 
         gamewin = getwindowrect()
         if gamewin:
@@ -63,14 +66,16 @@ class DiscordClient(discord.Client):
         else:
             print("Failed to capture game window!")
     
+
     # fast nuke is for our refresh, hence we're skipping our own stuff, since well if we edit our last message we're chilling.
     async def fastnuke(self):
         print("Wiping Frame")
         channel = self.get_channel(channel_id)
         
         messages = channel.history(limit=None, oldest_first=True)
+        
         async for msg in messages:
-            if not msg.author.name == "actes_plays_pokemon":
+            if channel.message_count > 1:
                 await msg.delete()
 
 
@@ -83,81 +88,80 @@ class DiscordClient(discord.Client):
         async for i in channel.history(limit=None):
             await i.delete()
 
+
     async def screenshot(self):
         print("Taking screenshot")
         channel = self.get_channel(channel_id)
 
-        gamewin = pyg.getWindowsWithTitle("mGBA")[0]
-        screen = pyg.screenshot().crop([gamewin.left + 8, gamewin.top + 52, gamewin.right - 8, gamewin.bottom - 8])
-        screen.save("frame.png")
+        gamewin = getwindowrect()
+        if gamewin:
+            screen = pyg.screenshot().crop(gamewin)
+            screen.save("frame.png")
 
-        frame = await channel.send(file=discord.File("frame.png"))
-        await asyncio.sleep(3)
-        await frame.delete()
+            frame = await channel.send(file=discord.File("frame.png"))
+            await asyncio.sleep(3)
+            await frame.delete()
+        else:
+            print("\nFailed to capture game window!")
 
         
     async def on_ready(self):
-        print("Ready for action")
+        print(f"\nReady for action, running:{self.command}")
         channel = self.get_channel(channel_id)
-        #await channel.send("Ready for action!")
+        
+        if "/game" in self.command:
+            await self.game()
 
-        # message_buffer = []
-        for x in message_buffer:
-            if x == "/game":
-                await self.game()
+        if "/nuke" in self.command:
+            await self.nuke()
+        
+        if "/ss" in self.command:
+            await self.screenshot()
 
-            if x == "/nuke":
-                await self.nuke()
+        if "/whoami" in self.command:
+            msg = await channel.send("whoami?")
+            print(msg.author.name)
+            await msg.delete()
+
+        if "/send-" in self.command:
             
-            if x == "/ss":
-                await self.screenshot()
+            breakdown = self.command.split("-")
+            breakdown.pop(0)
 
-            if "/send-" in x:
-                
-                breakdown = x.split("-")
-                breakdown.pop(0)
+            messages = []
 
-                messages = []
-
-                for msg in breakdown:
-                    print(f"message sent:{msg}")
-                    messages.append(await channel.send(msg))
-
-                await asyncio.sleep(5)
-
-                for y in messages:
-                    await y.delete()
+            for msg in breakdown:
+                print(f"message sent:{msg}")
+                messages.append(await channel.send(msg))
                 
 
-        print("Clearing buffer and closing connection")
-        
+            await asyncio.sleep(5)
 
-        message_buffer.clear()
+            for y in messages:
+                await y.delete()
+                
+                
+        print("Thread has finished, closing connection")
         
-        await self.close()
-        await self.http.close()
         await self.http.connector.close()
+        await self.http.close()
+        await self.close()
 
-        loop = asyncio.get_event_loop()
-        loop.stop()
-        loop.close()        
-        
-        
-        
-            
 
-# @client.event
-# async def on_ready():
-#     print("Ready for action")
-#     channel = client.get_channel(channel_id)
-#     #await channel.send("Ready for action!")
+def disc_Thread(command):
+    client = DiscordClient(intents=discord.Intents.default())
+    client.command = command
+    client.run(api_key, reconnect=False, log_handler=None)
 
-#     # message_buffer = []
-#     for x in message_buffer:
-#         print(f"message sent and popped off the buffer:{x}")
-#         await channel.send(x)
-#     await client.close()
 
+def command_and_wait(command):
+    thread = threading.Thread(target=disc_Thread,kwargs={"command": command})
+    thread.start()
+
+
+def first_time_setup():
+    chan = input("Enter channel id:")
+    chan
 
 def main():
     print("init")
@@ -167,47 +171,40 @@ def main():
     # thread1.start()
     gamer = False
 
+    
+
     while True:
 
         if gamer:
             message_buffer.append(message)
             client = DiscordClient(intents=discord.Intents.default())
             client.run(api_key, reconnect=False)
+
         else:
             message = input("Message here:")
-            
-            if message == "/game":
-                gamer = True
+            if message == "/whoami":
+                command_and_wait(message)
 
-            if message == 'kill':
+            if message == '/kill':
                 thread_killer = False
                 print("killed all threads")
             
             if message == "/ss":
-                message_buffer.append(message)
-                client = DiscordClient(intents=discord.Intents.default())
-                client.run(api_key, reconnect=False)
-
+                command_and_wait(message)
 
             if message == "/nuke":
-                message_buffer.append(message)
-                client = DiscordClient(intents=discord.Intents.default())
-                client.run(api_key, reconnect=False)
+                command_and_wait(message)
 
             if "/send-" in message:
+                command_and_wait(message)
 
-                message_buffer.append(message)
-                client = DiscordClient(intents=discord.Intents.default())
+            if message == "/play":
+                pass
 
-                client.run(api_key, reconnect=False)
-                                
-
+            if message == "/first":
+                first_time_setup()
                 
                 
-            
-
-            
-
 
 
 if __name__ == "__main__":
