492528e55f1ef46c95bfbf81e89725f4e5336c16
actes2 <101230792+actes2@users.noreply.github.com>
2024-11-26 10:39:53 -0600

    Update pokemon_api.py

--- main_program/app/pokemon_api.py
+++ main_program/app/pokemon_api.py
@@ -1,3 +1,20 @@
+"""pokemon_api.py
+
+This file is responsible for setting up our game window.
+
+We leverage the `discord` library to send a constant feed of our emulator screen to a 'edited' message in a specific channel.
+
+This is what is actually leveraged in our Docker instance
+
+## Anticipated .ENV
+
+```
+API_KEY=<Your API Key>
+BOT_NAME=<Your Bot Name>
+CHANNEL_ID=<Your channel ID for the Bot>
+```
+"""
+
 import pyautogui as pyg
 from typing import List
 import discord
@@ -10,14 +27,25 @@ from dotenv import load_dotenv
 import os
 import sys
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+
+# todo - add an enviornment variable for the bots name
 
 if not os.getenv("API_KEY"):
-    load_dotenv()
-    print("Loaded environment")
+    env_result = load_dotenv()
+    if not env_result:
+        logger.error("environment failed to load!")
+        exit(1)
 
-logger = logging.getLogger()
+    logger.info("Environment successfully loaded")
+
+
 api_key = os.getenv("API_KEY")
 channel_id = int(os.getenv("CHANNEL_ID"))
+bot_name = os.getenv("BOT_NAME")
 
 message_buffer = []
 
@@ -36,23 +64,22 @@ def is_game_up() -> bool:
     try:
         subprocess.check_output("wmctrl -lG | grep 'mGBA'", shell=True, text=True).split()
         return True
-    except:
+    except Exception:
         return False
-    
-    
+
 
 def getwindowrect(window_title="mGBA"):
     if operating_system == "windows":
         try:
             gamewin = pyg.getWindowsWithTitle("mGBA")[0]
             return [gamewin.left + 11, gamewin.top + 76, gamewin.right - 11, gamewin.bottom - 11]
-        except:
+        except Exception:
             return None
     else:
         try:
             output = subprocess.check_output("wmctrl -lG | grep 'mGBA'", shell=True, text=True).split()
             return [int(output[2]), int(output[3]) + 2, int(output[4]), int(output[5]) - 21]
-        except:
+        except Exception:
             return None
         
 
@@ -60,7 +87,7 @@ def action_queue_runner():
     while True:
         while action_queue:
             action = action_queue.pop(0)
-            print(f"Executing: {action}")
+            logger.info("Executing: {}".format(action))
             act_on_action(action=action)
             
 
@@ -118,7 +145,6 @@ def perform_game_action(action):
         if times > 10:
             times = 10
         for _ in range(0, times):
-            #sleep(0.5)
             action_queue.append(b_action[0])
     else:
         action_queue.append(action)
@@ -137,9 +163,13 @@ class DiscordClient(discord.Client):
         self.skipper = False
         self.command = ""
 
-
-    # we're gonna try and get it to just edit the last message with the new screenshot as to create a more 'game-esk' screen
     async def game(self):
+        """Game Runner
+
+        In this method, we render our game, by screenshotting our docker instances XSession, while cropping the game.
+
+        We then take this and edit our existing message, to pretend to be a "screen" of sorts.
+        """
 
         while True:
             channel = self.get_channel(channel_id)
@@ -162,7 +192,7 @@ class DiscordClient(discord.Client):
 
                 if msgcnt > 1:
                     for msg in all_messages:
-                        if not msg.author.name == "actes_plays_pokemon" and str(msg.content).startswith("!"):
+                        if not msg.author.name == bot_name and str(msg.content).startswith("!"):
                             t = threading.Thread(target=check_game_action, kwargs={"action": str(msg.content)})
                             t.start()
                     await self.fastnuke()
@@ -174,8 +204,6 @@ class DiscordClient(discord.Client):
                     l_msg = all_messages[0]
                     await l_msg.edit(attachments=[discord.File("frame.png")], content="Commands:\n\n!a !b !start !select !lb !rb !up !down !left !right\n\nIf you tag a + after a command followed by a number ex: 'up+5' the bot will run that command that many times!\n\n")
 
-                #await asyncio.sleep(1)
-
                 msgcnt = 0
                 all_messages: List[discord.Message] = []
 
@@ -184,40 +212,30 @@ class DiscordClient(discord.Client):
                     all_messages.append(x)
 
                 for msg in all_messages:
-                    if not msg.author.name == "actes_plays_pokemon" and str(msg.content).startswith("!"):
-                        
+                    if not msg.author.name == bot_name and str(msg.content).startswith("!"):
                         t = threading.Thread(target=check_game_action, kwargs={"action": str(msg.content)})
-                        t.start()#await check_game_action(str(msg.content))
-                
-                
+                        t.start()
                 await self.fastnuke()
-
-                
             else:
-                print("Failed to capture game window!")
-    
+                logger.error("Failed to capture game window!")
 
     # fast nuke is for our refresh, hence we're skipping our own stuff, since well if we edit our last message we're chilling.
     async def fastnuke(self):
-        print("Wiping Frame")
+        logger.info("Wiping Frame")
         channel = self.get_channel(channel_id)
-        
         msgcnt = 0
         all_messages: List[discord.Message] = []
 
         async for x in channel.history(limit=None):
             msgcnt += 1
             all_messages.append(x)
-
-        
         for msg in all_messages:
             if msgcnt > 1:
                 await msg.delete()
             msgcnt -= 1
 
-
     async def nuke(self):
-        print("Nuking channel")
+        logger.info("Nuking channel")
         channel = self.get_channel(channel_id)
         await channel.send("Nuking the entire channel in 5")
         await asyncio.sleep(5)
@@ -225,9 +243,8 @@ class DiscordClient(discord.Client):
         async for i in channel.history(limit=None):
             await i.delete()
 
-
     async def screenshot(self):
-        print("Taking screenshot")
+        logger.info("Taking screenshot")
         channel = self.get_channel(channel_id)
 
         gamewin = getwindowrect()
@@ -242,11 +259,10 @@ class DiscordClient(discord.Client):
             await asyncio.sleep(3)
             await frame.delete()
         else:
-            print("\nFailed to capture game window!")
-
+            logger.info("Failed to capture game window!")
         
     async def on_ready(self):
-        print(f"\nReady for action, running:{self.command}")
+        logger.info("Ready for action, running:{}".format(self.command))
         channel = self.get_channel(channel_id)
         
         if "/game" in self.command:
@@ -260,7 +276,7 @@ class DiscordClient(discord.Client):
 
         if "/whoami" in self.command:
             msg = await channel.send("whoami?")
-            print(msg.author.name)
+            logger.info(msg.author.name)
             await msg.delete()
 
         if "/send-" in self.command:
@@ -271,17 +287,15 @@ class DiscordClient(discord.Client):
             messages = []
 
             for msg in breakdown:
-                print(f"message sent:{msg}")
+                logger.info("message sent:{}".format(msg))
                 messages.append(await channel.send(msg))
-                
 
             await asyncio.sleep(5)
 
             for y in messages:
                 await y.delete()
                 
-                
-        print("Thread has finished, closing connection")
+        logger.info("Thread has finished, closing connection")
         
         await self.http.connector.close()
         await self.http.close()
@@ -295,14 +309,13 @@ def disc_Thread(command):
 
 
 def command_and_wait(command):
-    thread = threading.Thread(target=disc_Thread,kwargs={"command": command})
+    thread = threading.Thread(target=disc_Thread, kwargs={"command": command})
     thread.start()
 
 
 def game_runner():
-    #print(thread_killer)
     while thread_killer:
-        print("\ninit_game_loop\n")
+        logger.info("\ninit_game_loop\n")
         intents = discord.Intents.default()
         intents.message_content = True
 
@@ -310,7 +323,7 @@ def game_runner():
         client.command = "/game"
         client.run(api_key, reconnect=False, log_handler=None)
 
-    print("game_loop_exited!")
+    logger.info("\ngame_loop_exited!\n")
 
 
 def first_time_setup():
@@ -335,12 +348,12 @@ def start_game_window() -> bool:
         subprocess.check_output("/squashfs-root/AppRun /game/pkmn_ultravhak.gba &", shell=True, text=True)
         return True
     except Exception as e:
-        print(f"Failed to start subprocess:{e}")
+        logger.error("Failed to start subprocess:{}".format(e))
         return False
 
 
 def main():
-    print("init")
+    logger.info("init")
 
     thread_killer = False
 
@@ -348,92 +361,90 @@ def main():
     auto_start = os.getenv("AUTO")
 
     while True:
-            if auto_start == "start":
-                message = "/game"
-            else:
-                message = input("Message here:")
+        if auto_start == "start":
+            message = "/game"
+        else:
+            message = input("Message here:")
+        
+        if message == "/help":
+            tutorial = """
+            Pokemon_API driver for our container:
+
+            commands:
+
+            /first:\t is for configuring the API-KEY And Channel_ID in the local .env
+
+            /debug:\t is a dry-run /game. It just runs one frame and does all the game things.
+
+            /kill:\t I'm not even sure this works? It's a variable hardstop for telling all returning threads to commit neck.
+
+            /nuke:\t Deletes all messages in the channel in .env
+
+            /ss:\t Takes a screenshot and then removes it after 5 seconds
+
+            /exit:\t Exits the application and container
+
+            /send-#input#:\t Sends a message to the .env channel, which is delimited by -'s. Then deletes it after 5 seconds
+
+            /game:\t Runs our 'game-render-loop'
             
-            if message == "/help":
-                tutorial = """
-                Pokemon_API driver for our container:
+            """
 
-                commands:
+            logger.info(tutorial)
 
-                /first:\t is for configuring the API-KEY And Channel_ID in the local .env
+        if message == "/debug":
+            intents = discord.Intents.default()
+            intents.message_content = True
 
-                /debug:\t is a dry-run /game. It just runs one frame and does all the game things.
+            client = DiscordClient(intents=intents)
+            client.command = "/game"
+            client.run(api_key, reconnect=False)
 
-                /kill:\t I'm not even sure this works? It's a variable hardstop for telling all returning threads to commit neck.
+        if message == "/whoami":
+            command_and_wait(message)
 
-                /nuke:\t Deletes all messages in the channel in .env
+        if message == '/kill':
+            thread_killer = False
+            logger.info("Killing game thread!!")
+        
+        if message == "/ss":
+            command_and_wait(message)
 
-                /ss:\t Takes a screenshot and then removes it after 5 seconds
+        if message == "/nuke":
+            command_and_wait(message)
 
-                /exit:\t Exits the application and container
+        if "/send-" in message:
+            command_and_wait(message)
 
-                /send-#input#:\t Sends a message to the .env channel, which is delimited by -'s. Then deletes it after 5 seconds
+        if message == "/exit":
+            return
+        
+        if message == "/start_window":
+            thread = threading.Thread(target=start_game_window)
+            thread.start()
 
-                /game:\t Runs our 'game-render-loop'
-               
-                """
-
-                print(tutorial)
-
-
-            if message == "/debug":
-                intents = discord.Intents.default()
-                intents.message_content = True
-
-                client = DiscordClient(intents=intents)
-                client.command = "/game"
-                client.run(api_key, reconnect=False)
-
-            if message == "/whoami":
-                command_and_wait(message)
-
-            if message == '/kill':
-                thread_killer = False
-                print("Killing game thread!!")
-            
-            if message == "/ss":
-                command_and_wait(message)
-
-            if message == "/nuke":
-                command_and_wait(message)
-
-            if "/send-" in message:
-                command_and_wait(message)
-
-            if message == "/exit":
-                return
-            
-            if message == "/start_window":
+        if message == "/game":
+            if not is_game_up:
                 thread = threading.Thread(target=start_game_window)
                 thread.start()
 
-            if message == "/game":
-                if not is_game_up:
-                    thread = threading.Thread(target=start_game_window)
+            if is_game_up:
+                if not thread_killer:
+                    thread_killer = True
+
+                    thread = threading.Thread(target=game_runner)
                     thread.start()
-
-                if is_game_up:
-                    if not thread_killer:
-                        thread_killer = True
-
-                        thread = threading.Thread(target=game_runner)
-                        thread.start()
-                        print("Starting game thread!")
-                    else:
-                        print("Game thread already started!!")
+                    logger.info("Starting game thread!")
                 else:
-                    print("Game appears to actually not be up!")
-                    print("run /start_window and try again")
+                    logger.info("Game thread already started!!")
+            else:
+                logger.info("Game appears to actually not be up!")
+                logger.info("run /start_window and try again")
 
-            if message == "/first":
-                first_time_setup()
+        if message == "/first":
+            first_time_setup()
                 
                 
 if __name__ == "__main__":
-    print(f"Operating system is:{operating_system}")
+    logger.info(f"Operating system is:{operating_system}")
     main()
-    
